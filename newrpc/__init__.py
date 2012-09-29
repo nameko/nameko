9506cce211767085f848600551ade938a3c73ded
Edward George <edwardgeorge@gmail.com>
2012-09-29 18:35:30 +0100

    more reorganisation.

--- newrpc/__init__.py
+++ newrpc/__init__.py
@@ -1,41 +1,14 @@
 from kombu import Producer
 
 from newrpc import entities
+from newrpc import responses
 from newrpc import sending
 from newrpc.common import UIDGEN
 from newrpc.context import add_context_to_payload
 from newrpc.consuming import queue_iterator
 from newrpc.decorators import ensure
-from newrpc.exceptions import RemoteError
 
 DEFAULT_RPC_TIMEOUT = 10
-
-
-def ifirst(iter_, ack=False):
-    for i in iter_:
-        if ack:
-            i.ack()
-        return i
-
-
-def first(iter_, ack_all=False, ack_others=True):
-    ret = ifirst(iter_, ack=ack_all)
-    for i in iter_:
-        if ack_others:
-            i.ack()
-    return ret
-
-
-def last(iter_, ack_all=False, ack_others=True):
-    i = None
-    prev = None
-    for i in iter_:
-        if ack_others and prev is not None:
-            prev.ack()
-        prev = i
-    if ack_all:
-        i.ack()
-    return i
 
 
 def create_rpcpayload(context, method, args, msg_id=None):
@@ -48,19 +21,6 @@ def create_rpcpayload(context, method, args, msg_id=None):
     return msg_id, message
 
 
-def iter_rpcresponses(queue, channel=None, timeout=None, **kwargs):
-    qw = queue_iterator(queue, channel=channel, timeout=timeout, **kwargs)
-    for msg in qw:
-        data = msg.payload
-        if data['failure']:
-            raise RemoteError(**data['failure'])
-        elif data.get('ending', False):
-            msg.ack()
-            return
-        else:
-            yield msg
-
-
 @ensure
 def send_rpc(connection, context, exchange, topic, method, args,
         timeout=DEFAULT_RPC_TIMEOUT):
@@ -69,7 +29,9 @@ def send_rpc(connection, context, exchange, topic, method, args,
         queue = entities.get_reply_queue(msgid, channel=channel)
         queue.declare()
         sending.send_topic(connection, exchange, topic, payload)
-        return last(iter_rpcresponses(queue, timeout=timeout), ack_all=True)
+        iter_ = queue_iterator(queue, timeout=timeout)
+        iter_ = responses.iter_rpcresponses(iter_)
+        return responses.last(iter_, ack_all=True)
 
 
 @ensure
---
+++ newrpc/responses.py
@@ -0,0 +1,43 @@
+from newrpc.exceptions import RemoteError
+
+
+__all__ = ['ifirst', 'first', 'last', 'iter_rpcresponses', ]
+
+
+def ifirst(iter_, ack=False):
+    for i in iter_:
+        if ack:
+            i.ack()
+        return i
+
+
+def first(iter_, ack_all=False, ack_others=True):
+    ret = ifirst(iter_, ack=ack_all)
+    for i in iter_:
+        if ack_others:
+            i.ack()
+    return ret
+
+
+def last(iter_, ack_all=False, ack_others=True):
+    i = None
+    prev = None
+    for i in iter_:
+        if ack_others and prev is not None:
+            prev.ack()
+        prev = i
+    if ack_all:
+        i.ack()
+    return i
+
+
+def iter_rpcresponses(iter_):
+    for msg in iter_:
+        data = msg.payload
+        if data['failure']:
+            raise RemoteError(**data['failure'])
+        elif data.get('ending', False):
+            msg.ack()
+            return
+        else:
+            yield msg
