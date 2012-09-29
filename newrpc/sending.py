06dfa5407fe622ca3a26064a7b2b51bdbb1c3c1b
Edward George <edwardgeorge@gmail.com>
2012-09-29 19:40:21 +0100

    simplified reply() to re-use send_direct()

--- newrpc/sending.py
+++ newrpc/sending.py
@@ -62,22 +62,11 @@ def send_rpc(connection, context, exchange, topic, method, args,
         return responses.last(iter_, ack_all=True)
 
 
-@ensure
 def reply(connection, msg_id, replydata=None, failure=None, on_return=None):
     if on_return is not None:
         raise NotImplementedError('on_return is not implemented')
 
-    producer_kwargs = {}
-    publish_kwargs = {}
-    with connection.channel() as channel:
-        msg = {'result': replydata, 'failure': failure, 'ending': False, }
-        exchange = entities.get_reply_exchange(msg_id)
-        producer = Producer(channel,
-                exchange=exchange,
-                routing_key=msg_id,
-                on_return=on_return,
-                **producer_kwargs)
-        producer.declare()
-        producer.publish(msg, **publish_kwargs)
-        msg = {'result': None, 'failure': None, 'ending': True, }
-        producer.publish(msg, **publish_kwargs)
+    msg = {'result': replydata, 'failure': failure, 'ending': False, }
+    send_direct(connection, msg_id, msg)
+    msg = {'result': None, 'failure': None, 'ending': True, }
+    send_direct(connection, msg_id, msg)
