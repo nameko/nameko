5edaa2596c54c8a23e1ed0438ed3e0d62e331bf4
Edward George <edwardgeorge@gmail.com>
2012-09-30 17:24:58 +0100

    speed up tests by adding _get_many() for memory transport. this removes the default 1s polling interval with memory transport that makes tests slow.

--- newrpc/memory.py
+++ newrpc/memory.py
@@ -1,5 +1,58 @@
+import eventlet
+from eventlet.event import Event
+from eventlet.green import os
+from eventlet.green import select
 from eventlet.green import Queue
 from kombu.transport import memory as _memory
+
+
+class MultiQueueConsumer(object):
+    def __init__(self, queues):
+        self.cancelled = False
+        self.event = Event()
+        self.queues = queues
+
+    class Waiter(object):
+        def __init__(self, consumer, queue):
+            self.consumer = consumer
+            self.queue = queue
+
+        @property
+        def cancelled(self):
+            return self.consumer.cancelled
+
+        def switch(self, item):
+            if self.cancelled or self.consumer.event.ready():
+                self.queue.queue.appendleft(item)
+                self.queue._schedule_unlock()
+            else:
+                self.consumer.event.send((self.queue, item))
+
+        def kill(self, *exc_info):
+            if not self.cancelled and not self.consumer.event.ready():
+                self.consumer.event.send(exc=exc_info)
+
+    def wait(self, timeout=None, return_queue=False):
+        empty_queues = []
+        for q in self.queues:
+            try:
+                if return_queue:
+                    return q, q.get_nowait()
+                else:
+                    return q.get_nowait()
+            except Queue.Empty:
+                empty_queues.append(q)
+        for q in empty_queues:
+            q.getters.add(self.Waiter(self, q))
+        self.cancelled = False
+        try:
+            with eventlet.Timeout(timeout, exception=Queue.Empty):
+                if return_queue:
+                    return self.event.wait()
+                else:
+                    return self.event.wait()[1]
+        finally:
+            self.cancelled = True
 
 
 class Channel(_memory.Channel):
@@ -13,6 +66,13 @@ class Channel(_memory.Channel):
     def _put_fanout(self, exchange, message, **kwargs):
         for queue in self._fanout_queues.get(exchange, []):
             self._put(queue, message)
+
+    def _get_many(self, queues, timeout=None):
+        queues = [(self._queue_for(q), q) for q in queues]
+
+        consumer = MultiQueueConsumer([q[0] for q in queues])
+        queue, item = consumer.wait(timeout=timeout, return_queue=True)
+        return item, dict(queues)[queue]
 
 
 def patch():
