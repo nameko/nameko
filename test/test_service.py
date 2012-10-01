7523c01d5ee75d0223a997a82b1772cdeb914735
Edward George <edwardgeorge@gmail.com>
2012-10-01 15:54:50 +0100

    add standard service test for remoteerrors

--- test/test_service.py
+++ test/test_service.py
@@ -1,7 +1,9 @@
 import eventlet
 import mock
+import pytest
 
 from newrpc import context
+from newrpc import exceptions
 from newrpc import sending
 from newrpc import service
 
@@ -21,7 +23,31 @@ def test_service(connection, get_connection):
             ret = sending.send_rpc(conn, ctx,
                 'testrpc', 'test', 'test_method', args={'foo': 'bar', },
                 timeout=3)
+            assert m().test_method.called
             assert ret == {'foo': 'bar', }
+    finally:
+        srv.kill()
+        connection.release()
+
+
+def test_exceptions(connection, get_connection):
+    class Controller(object):
+        def test_method(self, context, **kwargs):
+            raise KeyError('foo')
+
+    srv = service.Service(Controller,
+            connection=connection,
+            exchange='testrpc', topic='test', )
+    srv.start()
+    eventlet.sleep(0)
+
+    try:
+        ctx = context.get_admin_context()
+        with get_connection() as conn:
+            with pytest.raises(exceptions.RemoteError):
+                sending.send_rpc(conn, ctx,
+                        'testrpc', 'test', 'test_method',
+                        args={})
     finally:
         srv.kill()
         connection.release()
