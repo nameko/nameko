a5556885ac22aa247680409782c32711a147e44b
David Szotten <davidszotten@gmail.com>
2013-04-29 12:18:20 +0100

    add test_reqs to manifest

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ test_requirements, dependency_links = parse_requirements(
 
 setup(
     name='nameko',
-    version='0.1.3',
+    version='0.1.4',
     description='service framework supporting multiple'
                 'messaging and RPC implementations',
     author='onefinestay',
