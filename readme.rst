9bf02616d49974baffdca85bb70000024370ebe7
David Szotten <davidszotten@gmail.com>
2013-04-29 12:07:46 +0100

    pull in new setup.py + mention license

--- readme.rst
+++ readme.rst
@@ -6,3 +6,9 @@ Nameko
 
 #. amber-brown mushroom with a slightly gelatinous coating that is used as an ingredient in miso soup and nabemono.
 #. rpc library intended as an (almost) drop-in replacement for nova.rpc
+
+
+License
+-------
+
+Apache 2.0. See LICENSE for details.
--- setup.py
+++ setup.py
@@ -1,20 +1,71 @@
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+from os.path import abspath, dirname, join
+import sys
 
 
-with open('requirements.txt', 'rb') as f:
-    requirements = [i.strip() for i in f]
+setup_dir = dirname(abspath(__file__))
 
 
-setup(name='nameko',
-    version='0.1.1',
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--cov', 'nameko',
+            join(setup_dir, 'test'),
+        ]
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+def parse_requirements(fn, dependency_links):
+    requirements = []
+    with open(fn, 'rb') as f:
+        for dep in f:
+            dep = dep.strip()
+            # need to make test_requirements.txt work with
+            # setuptools like it would work with `pip -r`
+            # URLs will not work, so we transform them to
+            # dependency_links and requirements
+            if dep.startswith('-e '):
+                dep = dep[3:]
+
+            if dep.startswith('git+'):
+                dependency_links.append(dep)
+                _, dep = dep.rsplit('#egg=', 1)
+                dep = dep.replace('-', '==', 1)
+            requirements.append(dep)
+
+    return requirements, dependency_links
+
+requirements, dependency_links = parse_requirements(
+    join(setup_dir, 'requirements.txt'), [])
+
+test_requirements, dependency_links = parse_requirements(
+    join(setup_dir, 'test_requirements.txt'),
+    dependency_links)
+
+setup(
+    name='nameko',
+    version='0.1.2',
     description='service framework supporting multiple'
                 'messaging and RPC implementations',
     author='onefinestay',
     author_email='engineering@onefinestay.com',
-    packages=['nameko', ],
+    url='http://github.com/onefinestay/nameko',
+    packages=find_packages(exclude=['test', 'test.*']),
     install_requires=requirements,
-    test_requires=['pytest>=2.2.4', 'mock>=1.0b1', ],
-    zip_safe=False,
+    tests_require=test_requirements,
+    dependency_links=dependency_links,
+    zip_safe=True,
+    cmdclass={'test': PyTest},
+    license='Apache License, Version 2.0',
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: MacOS :: MacOS X",
@@ -22,4 +73,6 @@ setup(name='nameko',
         "Programming Language :: Python :: 2.7",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Intended Audience :: Developers", ])
+        "Intended Audience :: Developers",
+    ]
+)
