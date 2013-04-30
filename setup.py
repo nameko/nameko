5352598599e224f224453f5da0aa5040da55e2db
Tony Bajan <tony.bajan@onefinestay.com>
2013-04-30 11:35:32 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ test_requirements, dependency_links = parse_requirements(
 
 setup(
     name='nameko',
-    version='0.1.5',
+    version='0.1.6',
     description='service framework supporting multiple'
                 'messaging and RPC implementations',
     author='onefinestay',
