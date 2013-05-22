7aaec9c6af59e49909fbd168f437c0ec4a0cf9c0
junkafarian <junkafarian@gmail.com>
2013-05-22 16:32:13 +0100

    Update Context to support both getitem and getattr

--- nameko/context.py
+++ nameko/context.py
@@ -3,38 +3,50 @@ import iso8601
 from nameko.common import UTCNOW, UIDGEN
 
 
-class Context(object):
+class Context(dict):
+
     def __init__(self, user_id, is_admin=None, roles=None,
             remote_address=None, timestamp=None,
             request_id=None, auth_token=None, **kwargs):
+
+        if isinstance(timestamp, basestring):
+            timestamp = iso8601.parse_date(timestamp)
+        self.timestamp = timestamp or UTCNOW()
+        self.request_id = request_id or UIDGEN()
+        self.update(kwargs)
+
         # we currently care little about what goes in here
         # from nova's context
         self.user_id = user_id
         self.is_admin = is_admin or False
         self.roles = roles or []
         self.remote_address = remote_address
-        if isinstance(timestamp, basestring):
-            timestamp = iso8601.parse_date(timestamp)
-        self.timestamp = timestamp or UTCNOW()
-        self.request_id = request_id or UIDGEN()
         self.auth_token = auth_token
-        self.extra_kwargs = kwargs
+
+    def __getattr__(self, name):
+        if name not in self:
+            raise AttributeError("Context has no attribute '{}'".format(name))
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
+
+    def __repr__(self):
+        return 'Context({})'.format(super(Context, self).__repr__())
 
     def to_dict(self):
         # TODO: convert to UTC if not?
+        res = self.copy()
+
         timestamp = self.timestamp.isoformat()
         timestamp = timestamp[:timestamp.index('+')]
-        res = {
-            'user_id': self.user_id,
-            'is_admin': self.is_admin,
-            'roles': self.roles,
-            'remote_address': self.remote_address,
+        res.update({
             'timestamp': timestamp,
-            'request_id': self.request_id,
-            'auth_token': self.auth_token,
             'project_id': None,
-        }
-        res.update(self.extra_kwargs)
+        })
         return res
 
     def add_to_message(self, message):
