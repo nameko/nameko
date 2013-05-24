dd1179a456431d4e2cc39c3aabb355ddf8fc632b
junkafarian <junkafarian@gmail.com>
2013-05-24 12:19:22 +0100

    responses unit tests

---
+++ test/test_responses.py
@@ -0,0 +1,28 @@
+from mock import Mock
+
+from nameko import responses
+
+
+def test_iter_rpcresponses():
+    response_list = [
+        Mock(payload={'id': 1, 'failure': False, 'ending': False}),
+        Mock(payload={'id': 2, 'failure': False, 'ending': False}),
+        Mock(payload={'id': 3, 'failure': False, 'ending': True}),
+    ]
+
+    iter_ = responses.iter_rpcresponses(response_list)
+    ret = responses.last(iter_)
+
+    # should be the message preceeding the `ending`
+    assert ret.payload['id'] == 2
+
+
+def test_iter_rpcresponses_ending_only():
+    response_list = [
+        Mock(payload={'id': 3, 'failure': False, 'ending': True}),
+    ]
+
+    iter_ = responses.iter_rpcresponses(response_list)
+
+    # should not include the ending message
+    assert list(iter_) == []
