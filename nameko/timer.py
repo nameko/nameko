c33d060652419a83817f771a7f125309666987bf
Jan-Klaas Kollhof <jan@kollhof.net>
2013-06-07 11:47:04 +0100

    adding docs

--- nameko/timer.py
+++ nameko/timer.py
@@ -13,22 +13,16 @@ _log = getLogger(__name__)
 
 
 intervals = WeakKeyDictionary()
-
 timers = WeakKeyDictionary()
-
-
-def set_interval(timer_method, interval):
-    _log.debug(
-        'update timer for %s with %ss interval',
-        timer_method, interval)
-    timers[timer_method].interval = interval
 
 
 def timer(interval, func=None):
     '''
-    Decorates a method as a timer which is called every `interval` seconds.
+    Decorates a method as a timer, which will be called every `interval` sec.
 
     Example::
+
+    class Foobar(object):
 
         @timer(interval=5)
         def handle_timer(self):
@@ -43,6 +37,9 @@ def timer(interval, func=None):
 
 
 class Timer(object):
+    ''' A timer object, which will call a given method repeatedly at a given
+    interval.
+    '''
     def __init__(self, interval, func):
         self.interval = interval
         self.func = func
@@ -50,12 +47,21 @@ class Timer(object):
         self.should_stop = Event()
 
     def start(self):
+        ''' Starts the timer in a separate green thread.
+
+        Once started it may be stopped using its `stop()` method.
+        '''
         self.gt = eventlet.spawn(self.run)
         _log.debug(
             'started timer for %s with %ss interval',
             self.func, self.interval)
 
     def run(self):
+        ''' Runs the interval loop.
+
+        This should not be called directly, rather the `start()` method
+        should be used.
+        '''
         while not self.should_stop.ready():
             start = time.time()
             try:
@@ -64,9 +70,12 @@ class Timer(object):
                 _log.error('error in timer handler: %s', e)
 
             sleep_time = max(self.interval - (time.time() - start), 0)
-            self.snooze(sleep_time)
+            self.sleep_or_stop(sleep_time)
 
-    def snooze(self, sleep_time):
+    def sleep_or_stop(self, sleep_time):
+        ''' Sleeps for `sleep_time` seconds or until a `should_stop` event
+        has been fired, whichever comes first.
+        '''
         try:
             with Timeout(sleep_time):
                 self.should_stop.wait()
@@ -75,16 +84,51 @@ class Timer(object):
             pass
 
     def stop(self):
+        ''' Gracefully stops the timer, waiting for it's timer_method
+        to complete if it is running.
+        '''
         self.should_stop.send(True)
         self.gt.wait()
 
 
+class NoTimerForMethdod(Exception):
+    pass
+
+
+def get_interval(timer_method):
+    ''' Returns the initial interval set for the decorated `timer_method`.
+
+    It will raise a NoTimerForMethdod if the `timer_method` was not
+    decorated with `@timer`.
+    '''
+    try:
+        return intervals[timer_method.im_func]
+    except KeyError:
+        raise NoTimerForMethdod(timer_method)
+
+
+def get_timer(timer_method):
+    ''' Returns the timer for a `@timer` decorated method.
+
+    It will raise a NoTimerForMethdod if the `timer_method` was not
+    decorated with `@timer`.
+    '''
+    try:
+        tmr = timers[timer_method]
+    except KeyError:
+        interval = get_interval(timer_method)
+        tmr = Timer(interval, timer_method)
+        timers[timer_method] = tmr
+
+    return tmr
+
+
 def get_timers(service):
-    for name, timer_method in inspect.getmembers(service, inspect.ismethod):
+    ''' Returns all the timers of all the methods decorated with `@timer`
+    on the `service` object.
+    '''
+    for _, timer_method in inspect.getmembers(service, inspect.ismethod):
         try:
-            interval = intervals[timer_method.im_func]
-            tmr = Timer(interval, timer_method)
-            timers[timer_method] = tmr
-            yield tmr
-        except KeyError:
+            yield get_timer(timer_method)
+        except NoTimerForMethdod:
             pass
