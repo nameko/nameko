b006e218c5c1980d7f45fd27492d3d1417c73d8d
junkafarian <junkafarian@gmail.com>
2013-06-20 18:56:51 +0100

    the timeout param to itermessages didn't do what we wanted. Use eventlet instead

--- nameko/consuming.py
+++ nameko/consuming.py
@@ -1,9 +1,12 @@
+import eventlet
 from kombu.common import itermessages
+from nameko.exceptions import WaiterTimeout
 
 
 def queue_iterator(queue, no_ack=False, timeout=None):
     channel = queue.channel
 
-    for _, msg in itermessages(channel.connection, channel,
-            queue, limit=None, no_ack=no_ack, timeout=timeout):
-        yield msg
+    with eventlet.Timeout(timeout, exception=WaiterTimeout()):
+        for _, msg in itermessages(channel.connection, channel, queue,
+                                   limit=None, no_ack=no_ack):
+            yield msg
