7fafbff48c567ec00f81011142c5839dc48743e1
Matt Bennett <matt@bennett.name>
2013-09-05 18:42:31 +0100

    RPC enhancements: - single consumer queue - error handling prototype

--- nameko/rpc.py
+++ nameko/rpc.py
@@ -2,13 +2,16 @@ from __future__ import absolute_import
 from functools import partial
 from logging import getLogger
 import uuid
+from weakref import WeakKeyDictionary
 
 from kombu import Connection, Exchange, Queue
 from kombu.pools import producers
 from kombu.common import itermessages, maybe_declare
 
-from nameko.messaging import ConsumeProvider
-from nameko.dependencies import dependency_decorator, AttributeDependency
+from nameko.exceptions import MethodNotFound, RemoteErrorWrapper
+from nameko.messaging import QueueConsumer
+from nameko.dependencies import (
+    dependency_decorator, AttributeDependency, DecoratorDependency)
 
 
 _log = getLogger(__name__)
@@ -18,62 +21,155 @@ _log = getLogger(__name__)
 def rpc():
     return RpcProvider()
 
-RPC_EXCHANGE = Exchange('nameko-rpc', durable=True)
-RPC_QUEUE_TEMPLATE = 'rpc-{}.{}'
+RPC_EXCHANGE = Exchange('nameko-rpc', durable=True, type="topic")
+RPC_QUEUE_TEMPLATE = 'rpc-{}'
 
 
-class RpcProvider(ConsumeProvider):
-    def __init__(self):
-        super(RpcProvider, self).__init__(queue=None, requeue_on_error=False)
+rpc_consumers = WeakKeyDictionary()
+
+
+def get_rpc_consumer(srv_ctx):
+    """ Get or create an RpcConsumer instance for the given ``srv_ctx``
+    """
+    if srv_ctx not in rpc_consumers:
+        rpc_consumer = RpcConsumer(srv_ctx)
+        rpc_consumers[srv_ctx] = rpc_consumer
+
+    return rpc_consumers[srv_ctx]
+
+
+class RpcConsumer(QueueConsumer):
+
+    def __init__(self, srv_ctx):
+        self._queue = None
+        self._providers = {}
+        self._srv_ctx = srv_ctx
+
+        amqp_uri = srv_ctx.config['amqp_uri']
+        max_workers = srv_ctx.max_workers
+        super(RpcConsumer, self).__init__(amqp_uri, max_workers)
+
+    def prepare_queue(self):
+        if self._queue is None:
+
+            service_name = self._srv_ctx.name
+            queue_name = RPC_QUEUE_TEMPLATE.format(service_name)
+            routing_key = '{}.*'.format(service_name)
+
+            self._queue = Queue(
+                queue_name,
+                exchange=RPC_EXCHANGE,
+                routing_key=routing_key,
+                durable=True)
+
+            self.add_consumer(self._queue, self.handle_message)
+
+    def register_provider(self, rpc_provider):
+        service_name = self._srv_ctx.name
+        key = '{}.{}'.format(service_name, rpc_provider.name)
+        self._providers[key] = rpc_provider
+
+    def unregister_provider(self, rpc_provider):
+        service_name = self._srv_ctx.name
+        key = '{}.{}'.format(service_name, rpc_provider.name)
+        try:
+            del self._providers[key]
+        except KeyError:
+            pass  # not registered
+
+    def handle_message(self, body, message):
+        routing_key = message.delivery_info['routing_key']
+        try:
+            provider = self._providers[routing_key]
+        except KeyError:
+            self.handle_method_not_found(body, message)
+        else:
+            srv_ctx = self._srv_ctx
+            provider.handle_message(srv_ctx, body, message)
+
+    def handle_method_not_found(self, body, message):
+        routing_key = message.delivery_info['routing_key']
+        method_name = routing_key.split(".")[-1]
+
+        try:  # raise to generate a sensible traceback
+            raise MethodNotFound(method_name)
+        except MethodNotFound as exc:
+            error = RemoteErrorWrapper(exc)
+
+        reply_to = message.properties['reply_to']
+        correlation_id = message.properties.get('correlation_id')
+        responder = Responder(reply_to, correlation_id)
+
+        srv_ctx = self._srv_ctx
+        responder.send_response(srv_ctx, None, error)
+        self.ack_message(message)
+
+
+class RpcProvider(DecoratorDependency):
 
     def start(self, srv_ctx):
-        queue_name = RPC_QUEUE_TEMPLATE.format(srv_ctx.name, self.name)
-        routing_key = '{}.{}'.format(srv_ctx.name, self.name)
+        rpc_consumer = get_rpc_consumer(srv_ctx)
+        rpc_consumer.register_provider(self)
+        rpc_consumer.prepare_queue()
 
-        self.queue = Queue(
-            queue_name,
-            exchange=RPC_EXCHANGE,
-            routing_key=routing_key,
-            durable=True)
+    def on_container_started(self, srv_ctx):
+        rpc_consumer = get_rpc_consumer(srv_ctx)
+        rpc_consumer.register_provider(self)
+        rpc_consumer.start()
 
-        super(RpcProvider, self).start(srv_ctx)
+    def stop(self, srv_ctx):
+        rpc_consumer = get_rpc_consumer(srv_ctx)
+        rpc_consumer.unregister_provider(self)
+        rpc_consumer.stop()  # waits for jobs?
 
     def handle_message(self, srv_ctx, body, message):
         args = body['args']
         kwargs = body['kwargs']
 
         reply_to = message.properties['reply_to']
-        responder = Responder(reply_to)
+        correlation_id = message.properties.get('correlation_id')
 
+        responder = Responder(reply_to, correlation_id)
         srv_ctx.container.spawn_worker(
             self, args, kwargs,
             handle_result=partial(self.handle_result, message, responder))
 
     def handle_result(self, message, responder, worker_ctx, result, exc):
-        responder.send_response(worker_ctx, result, exc)
-        super(RpcProvider, self).handle_result(
-            message, worker_ctx, result, exc)
+
+        error = None
+        if exc is not None:
+            error = RemoteErrorWrapper(exc)
+
+        responder.send_response(worker_ctx.srv_ctx, result, error)
+        rpc_consumer = get_rpc_consumer(worker_ctx.srv_ctx)
+        rpc_consumer.ack_message(message)
 
 
 class Responder(object):
-    def __init__(self, reply_to):
+    def __init__(self, reply_to, correlation_id):
         self.reply_to = reply_to
+        self.correlation_id = correlation_id
 
-    def send_response(self, worker_ctx, result, exc):
-        srv_ctx = worker_ctx.srv_ctx
+    def send_response(self, srv_ctx, result, error_wrapper):
 
         conn = Connection(srv_ctx.config['amqp_uri'])
 
         # TODO: if we use error codes outside the payload we would only
         # need to serialize the actual value
-        msg = {'result': result, 'error': exc}
+        # assumes result is json-serializable
+        error = None
+        if error_wrapper is not None:
+            error = error_wrapper.serialize()
+
+        msg = {'result': result, 'error': error}
 
         with producers[conn].acquire(block=True) as producer:
             # TODO: should we enable auto-retry,
             #      should that be an option in __init__?
 
             # all queues are bound to the anonymous direct exchange
-            producer.publish(msg, routing_key=self.reply_to)
+            producer.publish(msg, routing_key=self.reply_to,
+                             correlation_id=self.correlation_id)
 
 
 class Service(AttributeDependency):
@@ -136,7 +232,9 @@ class MethodProxy(object):
 
             resp_body, resp_message = next(resp_messages)
 
-            # TODO: need to do error handling
+            error = resp_body.get('error')
+            if error:
+                raise RemoteErrorWrapper.deserialize(error)
             return resp_body['result']
 
     def __str__(self):
--- test/test_rpc.py
+++ test/test_rpc.py
@@ -1,3 +1,36 @@
+import pytest
+
+from mock import Mock
+
+from nameko.exceptions import RemoteError
+from nameko.rpc import rpc, Service
+
+
+class ExampleError(Exception):
+    pass
+
+
+class ExampleService(object):
+
+    @rpc
+    def task_a(self, *args, **kwargs):
+        print "task_a", args, kwargs
+        return "result_a"
+
+    @rpc
+    def task_b(self, *args, **kwargs):
+        print "task_b", args, kwargs
+        return "result_b"
+
+    @rpc
+    def broken(self):
+        raise ExampleError("broken")
+
+    @rpc
+    def echo(self, *args, **kwargs):
+        return args, kwargs
+
+
 # def test_exceptions(get_connection):
 #     class Controller(object):
 #         def test_method(self, context, **kwargs):
@@ -18,3 +51,79 @@
 #             test.test_method_does_not_exist()
 #     finally:
 #         srv.kill()
+
+
+@pytest.fixture
+def service_proxy_factory(request):
+    def make_proxy(container, service_name):
+        worker_ctx = Mock(srv_ctx=container.ctx)
+        service_proxy = Service(service_name)
+        proxy = service_proxy.acquire_injection(worker_ctx)
+        return proxy
+    return make_proxy
+
+
+def test_rpc_consumer_creates_single_consumer(container_factory, rabbit_config,
+                                              rabbit_manager):
+    container = container_factory(ExampleService, rabbit_config)
+    container.start()
+
+    vhost = rabbit_config['vhost']
+    queues = rabbit_manager.get_queues(vhost)
+    assert len(queues) == 1
+    assert queues[0]['name'] == "rpc-exampleservice"
+
+
+def test_rpc_args_kwargs(container_factory, rabbit_config,
+                         service_proxy_factory):
+
+    container = container_factory(ExampleService, rabbit_config)
+    container.start()
+
+    proxy = service_proxy_factory(container, "exampleservice")
+
+    assert proxy.echo() == [[], {}]
+    assert proxy.echo("a", "b") == [["a", "b"], {}]
+    assert proxy.echo(foo="bar") == [[], {'foo': 'bar'}]
+    assert proxy.echo("arg", kwarg="kwarg") == [["arg"], {'kwarg': 'kwarg'}]
+
+
+def test_rpc_existing_method(container_factory, rabbit_config, rabbit_manager,
+                             service_proxy_factory):
+
+    container = container_factory(ExampleService, rabbit_config)
+    container.start()
+
+    proxy = service_proxy_factory(container, "exampleservice")
+
+    assert proxy.task_a() == "result_a"
+    assert proxy.task_b() == "result_b"
+
+
+def test_rpc_missing_method(container_factory, rabbit_config, rabbit_manager,
+                            service_proxy_factory):
+
+    container = container_factory(ExampleService, rabbit_config)
+    container.start()
+
+    proxy = service_proxy_factory(container, "exampleservice")
+
+    with pytest.raises(RemoteError) as exc_info:
+        proxy.task_c()
+    assert exc_info.value.exc_type == "MethodNotFound"
+
+
+def test_rpc_broken_method(container_factory, rabbit_config,
+                           rabbit_manager, service_proxy_factory):
+
+    container = container_factory(ExampleService, rabbit_config)
+    container.start()
+
+    proxy = service_proxy_factory(container, "exampleservice")
+
+    with pytest.raises(RemoteError) as exc_info:
+        proxy.broken()
+    assert exc_info.value.exc_type == "ExampleError"
+
+
+# test reply-to and correlation-id correct
