cd222601e864d14af30154937683cc8f69cac8a9
Jan-Klaas Kollhof <jan@kollhof.net>
2013-09-17 17:39:01 +0100

    adding docs

--- nameko/testing/__init__.py
+++ nameko/testing/__init__.py
@@ -5,6 +5,29 @@ from mock import patch, Mock
 
 @contextmanager
 def patch_attr_dependency(provider):
+    """ Patches an `AttributeDependency` provider's acquire_injection
+    such that it returns a `Mock` as the injection object.
+    The injection object will be yield by the contextmanager.
+
+    example:
+
+        class MyService(object):
+            dispatch=EventDispatcher()
+
+            @rpc
+            def foo(self):
+                self.dispatch(MyEvent())
+
+
+        def test_service_dispatches_event():
+
+            test_srv = TestContainer(MyService)
+
+            with patch_attr_dependency(MyService.dispatch) as dispatch:
+                test_srv.foo()
+                dispatch.assert_called_once_with(Any)
+
+    """
     injection = Mock()
 
     with patch.object(provider, 'acquire_injection') as acquire:
