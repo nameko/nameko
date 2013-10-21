1d29f65aa0dcbc89291553c93994cff13c9d657c
Matt Bennett <matt@bennett.name>
2013-10-21 11:34:22 +0100

    utils tests

--- test/test_testing.py
+++ test/test_testing.py
@@ -2,6 +2,7 @@ from mock import Mock
 
 from nameko.dependencies import AttributeDependency
 from nameko.testing import patch_attr_dependency
+from nameko.testing.utils import AnyInstanceOf
 
 
 def test_patch_attr_dependency():
@@ -19,3 +20,25 @@ def test_patch_attr_dependency():
     with patch_attr as injected_mock:
         assert attr.acquire_injection(None) is injected_mock
         assert isinstance(injected_mock, Mock)
+
+
+def test_any_instance_of():
+
+    assert "" == AnyInstanceOf(str)
+    assert 99 != AnyInstanceOf(str)
+
+    class Foo(object):
+        pass
+    foo = Foo()
+
+    assert foo == AnyInstanceOf(Foo)
+    assert foo == AnyInstanceOf(object)
+    assert foo != AnyInstanceOf(foo)
+
+    assert repr(AnyInstanceOf(str)) == "<AnyInstanceOf-str>"
+    assert repr(AnyInstanceOf(Foo)) == "<AnyInstanceOf-Foo>"
+
+    assert AnyInstanceOf == AnyInstanceOf(type)
+    assert str == AnyInstanceOf(type)
+    assert int == AnyInstanceOf(type)
+    assert type == AnyInstanceOf(type)
