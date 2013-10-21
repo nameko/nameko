5123e112bbcf5e1f3e39df24a4b126481a47ffb2
Matt Bennett <matt@bennett.name>
2013-10-21 17:01:26 +0100

    100% (via some extra nova unit tests)

--- test/test_nova.py
+++ test/test_nova.py
@@ -1,10 +1,12 @@
 import eventlet
 from kombu import Producer
 import mock
+import pytest
 
 from nameko import context
 from nameko import nova
 from nameko.consuming import queue_iterator
+from nameko.exceptions import RemoteError
 from nameko.responses import ifirst
 
 
@@ -24,6 +26,28 @@ def test_delegation_to_send_rpc():
         nova.call(
             connection=conn, context=ctx, topic=topic,
             msg=msg, timeout=timeout, options=options)
+
+        send_rpc.assert_called_with(
+            conn, context=ctx, exchange=exchange,
+            topic=topic, method=method, args=args,
+            timeout=timeout)
+
+
+def test_delegation_to_send_rpc_default_exchange():
+
+    conn = 'connection'
+    ctx = 'context'
+    topic = 'topic'
+    method = 'foobar'
+    args = 'args'
+    msg = dict(method=method, args=args)
+    timeout = 123
+    exchange = 'rpc'
+
+    with mock.patch('nameko.nova.send_rpc') as send_rpc:
+        nova.call(
+            connection=conn, context=ctx, topic=topic,
+            msg=msg, timeout=timeout)
 
         send_rpc.assert_called_with(
             conn, context=ctx, exchange=exchange,
@@ -64,6 +88,46 @@ def test_send_rpc(get_connection):
             timeout=3)
 
         assert resp == {'foo': 'bar', }
+
+    assert not g
+
+
+def test_send_rpc_errors(get_connection):
+    def response_greenthread():
+        with get_connection() as conn:
+            with conn.channel() as chan:
+                queue = nova.get_topic_queue(
+                    'test_rpc', 'test', channel=chan)
+                queue.declare()
+                msg = ifirst(queue_iterator(queue, no_ack=True, timeout=2))
+                msgid, ctx, method, args = nova.parse_message(msg.payload)
+
+                exchange = nova.get_reply_exchange(msgid)
+                producer = Producer(chan, exchange=exchange, routing_key=msgid)
+
+                exc = Exception('error')
+                failure = (type(exc).__name__, str(exc), "tb_placeholder")
+
+                msg = {'result': None, 'failure': failure, 'ending': False}
+                producer.publish(msg)
+                msg = {'result': None, 'failure': None, 'ending': True}
+                producer.publish(msg)
+
+    g = eventlet.spawn_n(response_greenthread)
+    eventlet.sleep(0)
+
+    with get_connection() as conn:
+        ctx = context.get_admin_context()
+
+        with pytest.raises(RemoteError):
+            nova.send_rpc(
+                conn,
+                context=ctx,
+                exchange='test_rpc',
+                topic='test',
+                method='test_method',
+                args={'foo': 'bar', },
+                timeout=3)
 
     assert not g
 
