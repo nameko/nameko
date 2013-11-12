b1f0d25df0b8ce8cda02c0cd483e942993a77cdf
Shaun Stanworth <shaun_stanworth@wordbank.com>
2013-11-12 18:02:42 +0000

    If constructed, executor auto-wraps function calls on the thing it wraps with a call to submit

--- nameko/parallel.py
+++ nameko/parallel.py
@@ -2,9 +2,21 @@ from nameko.dependencies import InjectionProvider
 
 
 class ParallelExecutor(object):
+    def __init__(self, to_wrap=None):
+        self.to_wrap = to_wrap
+
     @classmethod
     def submit(cls, func, *args, **kwargs):
         return func(*args, **kwargs)
+
+    def __getattr__(self, item):
+        # return something that, if used, will go via submit
+        wrapped_attribute = getattr(self.to_wrap, item)
+        if callable(wrapped_attribute):
+            def wrapped_function(*args, **kwargs):
+                ParallelExecutor.submit(wrapped_attribute, *args, **kwargs)
+            return wrapped_function
+        return wrapped_attribute
 
 
 class ParalleliseProvider(InjectionProvider):
--- test/test_parallel.py
+++ test/test_parallel.py
@@ -6,3 +6,13 @@ def test_parallel_executor_gives_submit():
     fn = Mock()
     ParallelExecutor.submit(fn, 1)
     fn.assert_called_with(1)
+
+
+def test_parallel_wrapped_calls():
+    to_wrap = Mock()
+    to_wrap.foo = 3
+    fn = Mock()
+    to_wrap.wrapped_call = fn
+    assert ParallelExecutor(to_wrap).foo == 3
+    ParallelExecutor(to_wrap).wrapped_call(2)
+    fn.assert_called_with(2)
