5031619c65016bd7b034c2e367fc1337bb65e243
Shaun Stanworth <shaun_stanworth@wordbank.com>
2013-11-13 12:00:45 +0000

    Use threads in parallel executor

--- nameko/parallel.py
+++ nameko/parallel.py
@@ -5,15 +5,24 @@ from nameko.dependencies import InjectionProvider
 class ParallelExecutor(_base.Executor):
     def __init__(self, thread_provider):
         self.thread_provider = thread_provider
+        self.spawned_threads = set()
 
     def submit(self, func, *args, **kwargs):
         def do_function_call():
             return func(*args, **kwargs)
         # TODO: Returning green thread, not future...
-        return self.thread_provider.spawn_managed_thread(do_function_call)
+        t = self.thread_provider.spawn_managed_thread(do_function_call)
+        self.spawned_threads.add(t)
+        return t
 
     def __call__(self, to_wrap):
         return ParallelWrapper(self, to_wrap)
+
+    def shutdown(self, wait=True):
+        # Wait for all spawned threads to have finished
+        if wait:
+            for thread in self.spawned_threads:
+                thread.wait()
 
 
 class ParallelWrapper(object):
@@ -31,9 +40,11 @@ class ParallelWrapper(object):
         return wrapped_attribute
 
     def __enter__(self):
+        self.executor.__enter__()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, *args, **kwargs):
+        self.executor.__exit__(*args, **kwargs)
         return
 
 
--- test/test_parallel.py
+++ test/test_parallel.py
@@ -1,17 +1,14 @@
 from mock import Mock
 from nameko.parallel import ParallelExecutor
-
-
-class SimpleThreadProvider(object):
-    def spawn_managed_thread(self, run_method):
-        # Runs synchronously, doesn't use a thread
-        return run_method()
+from nameko.service import ManagedThreadContainer
+from nameko.testing.utils import wait_for_call
 
 
 def test_parallel_executor_gives_submit():
     to_call = Mock()
-    ParallelExecutor(SimpleThreadProvider()).submit(to_call, 1)
-    to_call.assert_called_with(1)
+    ParallelExecutor(ManagedThreadContainer()).submit(to_call, 1)
+    with wait_for_call(5, to_call) as to_call_waited:
+        to_call_waited.assert_called_with(1)
 
 
 def test_parallel_wrapper_layer():
@@ -19,7 +16,7 @@ def test_parallel_wrapper_layer():
     to_wrap.wrapped_attribute = 2
     to_call = Mock()
     to_wrap.wrapped_call = to_call
-    executor = ParallelExecutor(SimpleThreadProvider())
+    executor = ParallelExecutor(ManagedThreadContainer())
 
     # Confirm wrapped call comes via submit
     orig_submit = executor.submit
@@ -32,16 +29,22 @@ def test_parallel_wrapper_layer():
 
     executor.submit = pass_through_submit
 
+    # Non-callables are returned immediately
     assert executor(to_wrap).wrapped_attribute == 2
+
+    # Callables must be waited for
     executor(to_wrap).wrapped_call(3)
-    to_call.assert_called_with(3)
+
+    with wait_for_call(5, to_call) as to_call_waited:
+        to_call_waited.assert_called_with(3)
     submit_check.assert_called_with()
 
 
 def test_parallel_executor_context_manager():
     to_call = Mock()
-    with ParallelExecutor(SimpleThreadProvider()) as execution_context:
+    with ParallelExecutor(ManagedThreadContainer()) as execution_context:
         execution_context.submit(to_call, 4)
+    # No waiting, the context manager handles that
     to_call.assert_called_with(4)
 
 
@@ -49,6 +52,7 @@ def test_parallel_executor_wrapped_cm():
     to_wrap = Mock()
     to_call = Mock()
     to_wrap.wrapped_call = to_call
-    with ParallelExecutor(SimpleThreadProvider())(to_wrap) as wrapped:
+    with ParallelExecutor(ManagedThreadContainer())(to_wrap) as wrapped:
         wrapped.wrapped_call(5)
+    # No waiting, the context manager handles that
     to_call.assert_called_with(5)
