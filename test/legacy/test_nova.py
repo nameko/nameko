ebc5270f7055efb0d5ab27e41e2d8f148a9ddf74
Shaun Stanworth <shaun.stanworth@onefinestay.com>
2013-11-27 10:42:18 +0000

    Remove nova TODOs

--- test/legacy/test_nova.py
+++ test/legacy/test_nova.py
@@ -129,7 +129,6 @@ def test_send_rpc_errors(get_connection):
                 args={'foo': 'bar', },
                 timeout=3)
 
-    # TODO This assertion can occasionally fail
     assert not g
 
 
@@ -176,5 +175,4 @@ def test_send_rpc_multi_message_reply_ignores_all_but_last(get_connection):
         assert resp == {'spam': 'shrub', }
     eventlet.sleep()
 
-    # TODO This assertion can occasionally fail
     assert not g
