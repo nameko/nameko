fc2dba8d164b7f31e683e8c5d0c7cd88c18cee97
Shaun Stanworth <shaun.stanworth@onefinestay.com>
2014-03-20 12:58:27 +0000

    Remove dedicated equality methods

--- nameko/nameko_doc/entities.py
+++ nameko/nameko_doc/entities.py
@@ -1,12 +1,20 @@
 import rst_render as rst
 
 
-class ServiceCollection(object):
+TITLE_LEVEL_FOR_SUBSECTIONS = 2
+
+
+class EquatableMixin(object):
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class ServiceCollection(EquatableMixin):
     def __init__(self, services=None):
         self.services = services or []
-
-    def __eq__(self, other):
-        return self.services == other.services
 
     def render(self, rst_page_printer):
         """ Render this service collection out """
@@ -15,25 +23,18 @@ class ServiceCollection(object):
             rst_page_printer.add_page(page)
 
 
-class ServiceDescription(object):
+class ServiceDescription(EquatableMixin):
     def __init__(self, name, module_path, class_name, sections=None):
         self.name = name
         self.module_path = module_path
         self.class_name = class_name
         self.sections = sections or []
 
-    def __eq__(self, other):
-        return (
-            self.name == other.name and
-            self.module_path == other.module_path and
-            self.class_name == other.class_name and
-            self.sections == other.sections
-        )
-
     def render_page(self):
         """ Render a service into a page """
         section_parts = [
-            section.render_section(self, 2) for section in self.sections
+            section.render_section(self, TITLE_LEVEL_FOR_SUBSECTIONS)
+            for section in self.sections
         ]
 
         page = rst.render_page(
@@ -52,12 +53,9 @@ class ServiceDescription(object):
         return page
 
 
-class ReferenceSection(object):
+class ReferenceSection(EquatableMixin):
     def __init__(self, references=None):
         self.references = references or []
-
-    def __eq__(self, other):
-        return self.references == other.references
 
     def render_section(self, service_description, this_level=1):
         aside = rst.render_see_also_section(
@@ -73,16 +71,10 @@ class ReferenceSection(object):
         return aside
 
 
-class ClassReference(object):
+class ClassReference(EquatableMixin):
     def __init__(self, title, reference_path):
         self.title = title
         self.reference_path = reference_path
-
-    def __eq__(self, other):
-        return (
-            self.title == other.title and
-            self.reference_path == other.reference_path
-        )
 
     def render_reference(self):
         """ Render a reference to a class under a given category/title """
@@ -94,16 +86,10 @@ class ClassReference(object):
         )
 
 
-class Section(object):
+class Section(EquatableMixin):
     def __init__(self, title, contents=None):
         self.title = title
         self.contents = contents or []
-
-    def __eq__(self, other):
-        return (
-            self.title == other.title and
-            self.contents == other.contents
-        )
 
     def render_section(self, service_description, this_level=1):
         sub_section_contents = [
@@ -121,16 +107,10 @@ class Section(object):
         return section
 
 
-class SingleMethod(object):
+class SingleMethod(EquatableMixin):
     def __init__(self, method_name, extras=None):
         self.method_name = method_name
         self.extras = extras or []
-
-    def __eq__(self, other):
-        return (
-            self.method_name == other.method_name and
-            self.extras == other.extras
-        )
 
     def render_section(self, service_description, this_level=1):
         method_path = '{}.{}.{}'.format(
@@ -150,7 +130,7 @@ class SingleMethod(object):
         return method_ref
 
 
-class ExtraInstruction(object):
+class ExtraInstruction(EquatableMixin):
     def __init__(self, title, content):
         self.title = title
         self.content = content
