3b9bc3e4bf876019b5e58236f3eed8d60daf13bb
Matt Bennett <matt@bennett.name>
2014-03-28 09:57:47 +0000

    extra test for name clash

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -9,7 +9,7 @@ class CustomError(Exception):
     pass
 
 
-@pytest.yield_fixture(autouse=True)
+@pytest.yield_fixture
 def registry():
     with patch('nameko.exceptions.registry', {}) as patched:
         yield patched
@@ -39,6 +39,7 @@ def test_deserialize_to_remote_error():
     assert deserialized.args == ("CustomError something went wrong",)
 
 
+@pytest.mark.usefixtures('registry')
 def test_deserialize_to_instance():
 
     # register Error as deserializable to an instance
@@ -51,3 +52,30 @@ def test_deserialize_to_instance():
     assert type(deserialized) == CustomError
     assert deserialized.message == "something went wrong"
     assert deserialized.args == ("something went wrong",)
+
+
+def test_exception_name_clash():
+
+    class MethodNotFound(Exception):
+        # application exception with clashing name
+        pass
+
+    exc = MethodNotFound('application error')
+    data = serialize(exc)
+
+    deserialized = deserialize(data)
+    assert type(deserialized) == RemoteError
+    assert deserialized.exc_type == "MethodNotFound"
+    assert deserialized.value == "application error"
+    assert deserialized.args == ("MethodNotFound application error",)
+
+    from nameko.exceptions import MethodNotFound as NamekoMethodNotFound
+
+    exc = NamekoMethodNotFound('missing')
+    data = serialize(exc)
+
+    deserialized = deserialize(data)
+    assert type(deserialized) == NamekoMethodNotFound
+    assert deserialized.message == "missing"
+    assert deserialized.args == ("missing",)
+
