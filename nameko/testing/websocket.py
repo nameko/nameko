f584d59f280d75b321aa1c38890e57e5d62d06e2
Armin Ronacher <armin.ronacher@active-4.com>
2014-12-11 14:49:31 +0000

    Added nameko websocket test helpers.

---
+++ nameko/testing/websocket.py
@@ -0,0 +1,69 @@
+from __future__ import absolute_import
+
+import uuid
+import json
+
+from eventlet.event import Event
+
+from nameko.exceptions import RemoteError
+
+
+def make_virtual_socket(host, port, path='/ws'):
+    from websocket import WebSocketApp
+
+    result_handlers = {}
+
+    class Socket(object):
+
+        def __init__(self):
+            self.events_received = []
+
+        def rpc(self, _method, **data):
+            id = str(uuid.uuid4())
+            event = Event()
+            result_handlers[id] = event.send
+            ws_app.send(json.dumps({
+                'method': _method,
+                'data': data,
+                'correlation_id': id,
+            }))
+
+            rv = event.wait()
+            if rv['success']:
+                return rv['data']
+            raise RemoteError(rv['error']['type'], rv['error']['message'])
+
+    sock = Socket()
+
+    def on_message(ws, message):
+        msg = json.loads(message)
+        if msg['type'] == 'event':
+            sock.events_received.append((msg['event'], msg['data']))
+        elif msg['type'] == 'result':
+            result_id = msg['correlation_id']
+            handler = result_handlers.pop(result_id, None)
+            if handler is not None:
+                handler(msg)
+
+    ready_event = Event()
+
+    def on_open(ws):
+        ready_event.send(None)
+
+    def on_error(ws, err):
+        ready_event.send(err)
+
+    ws_app = WebSocketApp(
+        'ws://%s:%d%s' % (host, port, path),
+        on_message=on_message,
+        on_open=on_open,
+        on_error=on_error,
+    )
+
+    def connect_socket():
+        err = ready_event.wait()
+        if err is not None:
+            raise err
+        return sock
+
+    return ws_app, connect_socket
