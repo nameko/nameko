6ffde7477d0e5e76935335784a43dec9a1b5dbb9
Armin Ronacher <armin.ronacher@active-4.com>
2014-12-11 13:53:40 +0000

    Added post test

--- test/test_web.py
+++ test/test_web.py
@@ -1,3 +1,4 @@
+import json
 from nameko.web.handlers import http
 
 
@@ -7,8 +8,12 @@ class ExampleService(object):
     def do_foo(self, bar):
         return {'value': bar}
 
+    @http('POST', '/post')
+    def do_post(self, value):
+        return {'value': value}
 
-def test_simple(container_factory, web_config, web_session):
+
+def test_simple_rpc(container_factory, web_config, web_session):
     container = container_factory(ExampleService, web_config)
     container.start()
 
@@ -17,3 +22,22 @@ def test_simple(container_factory, web_config, web_session):
 
     rv = web_session.get('/foo/something')
     assert rv.status_code == 404
+
+
+def test_post_rpc(container_factory, web_config, web_session):
+    container = container_factory(ExampleService, web_config)
+    container.start()
+
+    rv = web_session.post('/post', json={
+        'value': 23,
+    })
+    assert rv.json() == {'data': {'value': 23}, 'success': True}
+
+    rv = web_session.post('/post', json={
+        'value': 23,
+        'extra': []
+    })
+    resp = rv.json()
+    assert rv.status_code == 400
+    assert not resp['success']
+    assert resp['error']['type'] == 'nameko.exceptions.IncorrectSignature'
