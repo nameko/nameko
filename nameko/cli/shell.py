f319b9a3f40701448322fe00c06da7ae39e5a3c3
David Szotten <davidszotten@gmail.com>
2014-12-24 16:41:28 +0000

    first part of shell

--- nameko/cli/shell.py
+++ nameko/cli/shell.py
@@ -1,5 +1,10 @@
-def main(args):
-    print "shell", args
+import code
+import os
+import sys
+from types import ModuleType
+
+from nameko.standalone.rpc import ClusterRpcProxy
+from nameko.standalone.events import event_dispatcher
 
 
 def init_parser(parser):
@@ -7,3 +12,44 @@ def init_parser(parser):
         '--broker', default='amqp://guest:guest@localhost:5672/nameko',
         help='RabbitMQ broker url')
     return parser
+
+
+def make_nameko_helper(config):
+    """Create a fake module that provides some convenient access to nameko
+    standalone functionality for interactive shell usage.
+    """
+    module = ModuleType('nameko')
+    module.__doc__ = """Nameko shell helper for making rpc calls and dispaching
+events.
+
+Usage:
+    >>> n.rpc.service.method()
+    "reply"
+
+    >>> n.dispatch_event('service', 'event_type', 'event_data')
+"""
+    module.rpc = ClusterRpcProxy(config).start()
+    module.dispatch_event = event_dispatcher(config)
+    module.config = config
+    return module
+
+
+def main(args):
+    banner = 'Nameko Python %s shell on %s\nBroker: %s' % (
+        sys.version,
+        sys.platform,
+        args.broker.encode('utf-8'),
+    )
+    config = {'AMQP_URI': args.broker}
+
+    ctx = {}
+    ctx['n'] = make_nameko_helper(config)
+
+    # Support the regular Python interpreter startup script if someone
+    # is using it.
+    startup = os.environ.get('PYTHONSTARTUP')
+    if startup and os.path.isfile(startup):
+        with open(startup, 'r') as f:
+            eval(compile(f.read(), startup, 'exec'), ctx)
+
+    code.interact(banner=banner, local=ctx)
