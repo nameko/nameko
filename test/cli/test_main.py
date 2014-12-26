e751991545e35925d4ed37aaf0441f078ed733ac
David Szotten <davidszotten@gmail.com>
2014-12-26 19:51:47 +0000

    tidy up test_main and add flag test

--- test/cli/test_main.py
+++ test/cli/test_main.py
@@ -1,33 +1,53 @@
-from contextlib import contextmanager
 import sys
 
 from mock import patch
 import pytest
 
-from nameko.cli.main import main
+from nameko.cli.main import main, setup_parser
 from nameko.cli.exceptions import CommandError
 
 
-@contextmanager
-def fake_argv(argv):
-    with patch.object(sys, 'argv', ['nameko'] + argv
+@pytest.yield_fixture(autouse=True)
+def fake_argv():
+    with patch.object(
+        sys,
+        'argv',
+        [
+            'nameko',
+            'run',
+            '--broker',
+            'my_broker',
+            'test.sample:Service',
+            ],
     ):
         yield
 
 
 def test_run():
-    with fake_argv([ 'run', '--broker', 'my_broker', 'test.sample:Service']):
-        with patch('nameko.cli.main.run.main') as run:
-            main()
+    with patch('nameko.cli.main.run.main') as run:
+        main()
     assert run.call_count == 1
     (args,), _ = run.call_args
     assert args.broker == 'my_broker'
 
 
 def test_error(capsys):
-    with fake_argv([ 'run', '--broker', 'my_broker', 'test.sample:Service']):
-        with patch('nameko.cli.main.run.main') as run:
-            run.side_effect = CommandError('boom')
-            main()
+    with patch('nameko.cli.main.run.main') as run:
+        run.side_effect = CommandError('boom')
+        main()
     out, _ = capsys.readouterr()
     assert out.strip() == 'Error: boom'
+
+
+@pytest.mark.parametrize(('param', 'value'), (
+    (None, None),
+    ('--rlwrap', True),
+    ('--no-rlwrap', False),
+))
+def test_flag_action(param, value):
+    parser = setup_parser()
+    args = ['backdoor', 0]
+    if param is not None:
+        args.append(param)
+    parsed = parser.parse_args(args)
+    assert parsed.rlwrap is value
