86ab5e91d4e3b4f4010070d69e983ca065fe3541
David Szotten <davidszotten@gmail.com>
2015-01-27 16:03:14 +0000

    move back to standalone

--- nameko/events.py
+++ nameko/events.py
@@ -32,12 +32,10 @@ from __future__ import absolute_import
 from logging import getLogger
 import uuid
 
-from kombu import Connection, Exchange, Queue
-from kombu.pools import producers, connections
+from kombu import Queue
 
-from nameko.constants import DEFAULT_RETRY_POLICY
-from nameko.messaging import (
-    Publisher, PERSISTENT, Consumer, AMQP_URI_CONFIG_KEY)
+from nameko.standalone.events import get_event_exchange, event_dispatcher
+from nameko.messaging import Publisher, Consumer
 
 
 SERVICE_POOL = "service_pool"
@@ -45,49 +43,6 @@ SINGLETON = "singleton"
 BROADCAST = "broadcast"
 
 _log = getLogger(__name__)
-
-
-def get_event_exchange(service_name):
-    """ Get an exchange for ``service_name`` events.
-    """
-    exchange_name = "{}.events".format(service_name)
-    exchange = Exchange(
-        exchange_name, type='topic', durable=True, auto_delete=True,
-        delivery_mode=PERSISTENT)
-
-    return exchange
-
-
-def event_dispatcher(nameko_config, **kwargs):
-    """ Returns a function that dispatches events claiming to originate from
-    a service called `container_service_name`.
-
-    Enables services not hosted by nameko to dispatch events into a nameko
-    cluster.
-    """
-
-    kwargs = kwargs.copy()
-    retry = kwargs.pop('retry', True)
-    retry_policy = kwargs.pop('retry_policy', DEFAULT_RETRY_POLICY)
-
-    def dispatch(service_name, event_type, event_data):
-        conn = Connection(nameko_config[AMQP_URI_CONFIG_KEY])
-
-        exchange = get_event_exchange(service_name)
-
-        with connections[conn].acquire(block=True) as connection:
-            exchange.maybe_bind(connection)
-            with producers[conn].acquire(block=True) as producer:
-                msg = event_data
-                routing_key = event_type
-                producer.publish(
-                    msg,
-                    exchange=exchange,
-                    routing_key=routing_key,
-                    retry=retry,
-                    retry_policy=retry_policy,
-                    **kwargs)
-    return dispatch
 
 
 class EventHandlerConfigurationError(Exception):
--- nameko/standalone/events.py
+++ nameko/standalone/events.py
@@ -1,2 +1,48 @@
-from nameko.events import event_dispatcher
-event_dispatcher  # TODO
+from kombu import Connection, Exchange
+from kombu.pools import producers, connections
+
+from nameko.constants import DEFAULT_RETRY_POLICY
+from nameko.messaging import PERSISTENT, AMQP_URI_CONFIG_KEY
+
+
+def get_event_exchange(service_name):
+    """ Get an exchange for ``service_name`` events.
+    """
+    exchange_name = "{}.events".format(service_name)
+    exchange = Exchange(
+        exchange_name, type='topic', durable=True, auto_delete=True,
+        delivery_mode=PERSISTENT)
+
+    return exchange
+
+
+def event_dispatcher(nameko_config, **kwargs):
+    """ Returns a function that dispatches events claiming to originate from
+    a service called `container_service_name`.
+
+    Enables services not hosted by nameko to dispatch events into a nameko
+    cluster.
+    """
+
+    kwargs = kwargs.copy()
+    retry = kwargs.pop('retry', True)
+    retry_policy = kwargs.pop('retry_policy', DEFAULT_RETRY_POLICY)
+
+    def dispatch(service_name, event_type, event_data):
+        conn = Connection(nameko_config[AMQP_URI_CONFIG_KEY])
+
+        exchange = get_event_exchange(service_name)
+
+        with connections[conn].acquire(block=True) as connection:
+            exchange.maybe_bind(connection)
+            with producers[conn].acquire(block=True) as producer:
+                msg = event_data
+                routing_key = event_type
+                producer.publish(
+                    msg,
+                    exchange=exchange,
+                    routing_key=routing_key,
+                    retry=retry,
+                    retry_policy=retry_policy,
+                    **kwargs)
+    return dispatch
