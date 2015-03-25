dd6c9c79cf24b8e5b614c704787c61cce0e8976a
David Szotten <davidszotten@gmail.com>
2015-03-25 17:13:43 +0000

    http entrypoint to take a requset arg

--- test/web/test_server.py
+++ test/web/test_server.py
@@ -10,11 +10,11 @@ class ExampleService(object):
     name = "exampleservice"
 
     @http('GET', '/')
-    def do_index(self):
+    def do_index(self, request):
         return ''
 
     @http('GET', '/large')
-    def do_large(self):
+    def do_large(self, request):
         # more than a buffer's worth
         return 'x' * (10**6)
 
