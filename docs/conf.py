4c0769dd7ff329c4ecd57cd217bdb23c77f6a51d
Matt Bennett <matt@bennett.name>
2015-03-30 17:51:33 +0100

    don't rely on setuptools

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sphinx_nameko_theme
 import pkg_resources
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -112,7 +113,8 @@ html_theme = 'nameko'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+# Travis doesn't support the sphinx_themes setuptools entry point
+html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
