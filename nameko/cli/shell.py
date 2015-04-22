84f200c336322394ac342ec5dcf6f9a484243ef7
Teun Zengerink <t.zengerink@gmail.com>
2015-04-22 14:57:39 +0200

    added option to run shell in bpython or ipython

--- nameko/cli/shell.py
+++ nameko/cli/shell.py
@@ -13,10 +13,19 @@ from nameko.standalone.rpc import ClusterRpcProxy
 from nameko.standalone.events import event_dispatcher
 
 
+SHELLS = ['bpython', 'ipython']
+
+
 def init_parser(parser):
     parser.add_argument(
         '--broker', default='amqp://guest:guest@localhost',
         help='RabbitMQ broker url')
+    parser.add_argument(
+        '--interface', choices=SHELLS,
+        help='Specify an interactive interpreter interface.')
+    parser.add_argument(
+        '--plain', action='store_true',
+        help='Use the regular Python interpreter.')
     return parser
 
 
@@ -42,6 +51,27 @@ Usage:
     return module
 
 
+def bpython(banner, local):
+    import bpython
+    bpython.embed(banner=banner, locals_=local)
+
+
+def ipython(banner, local):
+    from IPython import embed
+    embed(banner1=banner, user_ns=local)
+
+
+def run_shell(shell=None, banner=None, local=None):
+    available_shells = [shell] if shell else SHELLS
+
+    for shell in available_shells:
+        try:
+            return globals()[shell](banner, local)
+        except ImportError:
+            pass
+    raise ImportError
+
+
 def main(args):
     banner = 'Nameko Python %s shell on %s\nBroker: %s' % (
         sys.version,
@@ -53,11 +83,17 @@ def main(args):
     ctx = {}
     ctx['n'] = make_nameko_helper(config)
 
-    # Support the regular Python interpreter startup script if someone
-    # is using it.
-    startup = os.environ.get('PYTHONSTARTUP')
-    if startup and os.path.isfile(startup):
-        with open(startup, 'r') as f:
-            eval(compile(f.read(), startup, 'exec'), ctx)
+    try:
+        if args.plain:
+            raise ImportError
 
-    code.interact(banner=banner, local=ctx)
+        run_shell(shell=args.interface, banner=banner, local=ctx)
+    except ImportError:
+        # Support the regular Python interpreter startup script if someone
+        # is using it.
+        startup = os.environ.get('PYTHONSTARTUP')
+        if startup and os.path.isfile(startup):
+            with open(startup, 'r') as f:
+                eval(compile(f.read(), startup, 'exec'), ctx)
+
+        code.interact(banner=banner, local=ctx)
