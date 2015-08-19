831c6f149d66a3a74b64fb628fa5f0e844bb237a
Matt Bennett <matt@bennett.name>
2015-08-19 17:04:14 +0100

    Don't enforce coverage of open_connections check until I can figure out how to make coverage play with testdir

--- nameko/testing/pytest.py
+++ nameko/testing/pytest.py
@@ -91,7 +91,7 @@ def rabbit_config(request, rabbit_manager):
     open_connections = [
         conn for conn in connections if conn['state'] != "closed"
     ]
-    if open_connections:
+    if open_connections:  # pragma: no cover
         count = len(open_connections)
         raise RuntimeError("{} rabbit connection(s) left open.".format(count))
 
