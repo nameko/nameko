6f16073bfff0f5e451d9ea37131e00abfe6a65a7
Matt Bennett <matt@bennett.name>
2015-09-26 23:00:12 +0100

    coverage 4.0 released

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     extras_require={
         'dev': [
-            "coverage==4.0b3",
+            "coverage==4.0",
             "flake8==2.1.0",
             "mccabe==0.3",
             "pep8==1.6.1",
