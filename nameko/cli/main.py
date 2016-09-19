b4a81faa69439eee489bb48ac9de826f630eb091
Jakub Borys <jakub.borys@gmail.com>
2016-09-19 10:00:10 +0100

    Moved implementation to utils.

--- nameko/cli/main.py
+++ nameko/cli/main.py
@@ -1,18 +1,10 @@
 from __future__ import print_function
 
 import argparse
-import os
-import re
-import yaml
 
 from nameko.exceptions import CommandError, ConfigurationError
 from . import backdoor, run, shell
-
-
-ENV_VAR_PATTERN = re.compile(
-    r'\$\{([a-zA-Z][^}:\s]+)(:([^}]+))?\}', re.MULTILINE | re.DOTALL
-)
-IMPLICIT_MATCH_PATTERN = re.compile(r'.*\$\{.*\}.*', re.MULTILINE)
+from nameko.utils import setup_yaml_parser
 
 
 def setup_parser():
@@ -26,23 +18,6 @@ def setup_parser():
         module.init_parser(module_parser)
         module_parser.set_defaults(main=module.main)
     return parser
-
-
-def replace_env_var(match):
-    env_var, _, default = match.groups()
-    if default == 'null':
-        default = None
-    return os.environ.get(env_var, default)
-
-
-def env_var_constructor(loader, node):
-    value = loader.construct_scalar(node)
-    return ENV_VAR_PATTERN.sub(replace_env_var, value)
-
-
-def setup_yaml_parser():
-    yaml.add_constructor('!env_var', env_var_constructor)
-    yaml.add_implicit_resolver("!env_var", IMPLICIT_MATCH_PATTERN)
 
 
 def main():
--- test/cli/test_main.py
+++ test/cli/test_main.py
@@ -5,8 +5,9 @@ import yaml
 from mock import patch
 import pytest
 
-from nameko.cli.main import main, setup_parser, setup_yaml_parser
+from nameko.cli.main import main, setup_parser
 from nameko.exceptions import CommandError, ConfigurationError
+from nameko.utils import setup_yaml_parser
 
 
 @pytest.yield_fixture(autouse=True)
@@ -57,9 +58,13 @@ def test_flag_action(param, value):
 
 
 @pytest.mark.parametrize(('yaml_config', 'env_vars', 'expected_config'), [
-    # will use default value
+    # no default value, no env value
+    ('FOO: ${BAR}', {}, {'FOO': ''}),
+    # use default value if env value not provided
     ('FOO: ${BAR:foo}', {}, {'FOO': 'foo'}),
-    # will use supplied value
+    # use env value
+    ('FOO: ${BAR}', {'BAR': 'bar'}, {'FOO': 'bar'}),
+    # use env value instead of default
     ('FOO: ${BAR:foo}', {'BAR': 'bar'}, {'FOO': 'bar'}),
     # handles multi line
     (
@@ -70,14 +75,19 @@ def test_flag_action(param, value):
         {'BAR': 'bar', 'FOO': 'foo'},
         {'FOO': 'bar', 'BAR': 'foo'}
     ),
-    # quoted values without explicit resolver
+    # quoted values don't work without explicit resolver
     ('FOO: "${BAR:foo}"', {'BAR': 'bar'}, {'FOO': '${BAR:foo}'}),
-    # quoted values with explicit resolver
-    ('FOO: !env_var "${BAR:foo}"', {'BAR': 'bar'}, {'FOO': 'bar'})
+    # quoted values work only with explicit resolver
+    ('FOO: !env_var "${BAR:foo}"', {'BAR': 'bar'}, {'FOO': 'bar'}),
+
 
 ])
 def test_environment_vars_in_config(yaml_config, env_vars, expected_config):
     setup_yaml_parser()
+
+    for val in ['FOO', 'BAR']:
+        if os.environ.get(val):
+            del os.environ[val]
 
     for key, val in env_vars.items():
         os.environ[key] = val
