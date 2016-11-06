b2f2d4778c8b84d9c92b1f22b433dbe9d474554c
David Szotten <davidszotten@gmail.com>
2016-11-06 11:46:42 +0000

    disallow new kombu until we can test it

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=[
         "eventlet>=0.16.1",
-        "kombu>=3.0.1",
+        "kombu>=3.0.1,<4",
         "mock>=1.2",
         "path.py>=6.2",
         "pyyaml>=3.10",
