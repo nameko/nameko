e4d76ca6293bcb15b7506740d828e08fbbe6a0b9
Matt Bennett <matt@bennett.name>
2016-11-18 11:19:07 +0000

    avoid using __enter__ when patching

--- test/conftest.py
+++ test/conftest.py
@@ -19,16 +19,19 @@ TOXIPROXY_PORT = 8474
 @pytest.yield_fixture
 def mock_producer():
     with patch('nameko.amqp.producers') as patched:
-        producer = patched[ANY].acquire().__enter__()
-        # normal behaviour is for no messages to be returned
-        producer.channel.returned_messages.get_nowait.side_effect = queue.Empty
-        yield producer
+        with patched[ANY].acquire() as producer:
+            # normal behaviour is for no messages to be returned
+            producer.channel.returned_messages.get_nowait.side_effect = (
+                queue.Empty
+            )
+            yield producer
 
 
 @pytest.yield_fixture
 def mock_connection():
     with patch('nameko.amqp.connections') as patched:
-        yield patched[ANY].acquire().__enter__()
+        with patched[ANY].acquire() as connection:
+            yield connection
 
 
 @pytest.yield_fixture(scope='session')
