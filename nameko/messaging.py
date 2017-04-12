16a589a2bb2adb5ef105d69e2687c73b994ccd06
Matt Bennett <matt@bennett.name>
2017-04-12 17:57:10 +0100

    add useful identifier for managed thread

--- nameko/messaging.py
+++ nameko/messaging.py
@@ -387,8 +387,11 @@ class QueueConsumer(SharedExtension, ProviderCollector, ConsumerMixin):
         return Connection(self.amqp_uri, heartbeat=heartbeat)
 
     def handle_message(self, provider, body, message):
+        ident = "{}.handle_message[{}]".format(
+            type(provider).__name__, message.delivery_info['routing_key']
+        )
         self.container.spawn_managed_thread(
-            lambda: provider.handle_message(body, message)
+            lambda: provider.handle_message(body, message), identifier=ident
         )
 
     def get_consumers(self, consumer_cls, channel):
