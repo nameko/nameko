95a1722e7b6a205a3d3559043c90674a62461b2f
defat <defat@ukr.net>
2017-05-17 16:48:28 +0300

    Config dependency provider reverted

--- nameko/dependency_providers.py
+++ nameko/dependency_providers.py
@@ -8,11 +8,5 @@ class Config(DependencyProvider):
     """ Dependency provider for accessing configuration values.
     """
 
-    def __init__(self, section=None):
-        self.section = section
-
     def get_dependency(self, worker_ctx):
-        config = self.container.config.copy()
-        if self.section:
-            return config[self.section]
-        return config
+        return self.container.config.copy()
