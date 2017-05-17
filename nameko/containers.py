bac6f2ed29f410bc92f433db91770a4a78daa24d
defat <defat@ukr.net>
2017-05-17 16:01:02 +0300

    Line length fixed

--- nameko/containers.py
+++ nameko/containers.py
@@ -369,7 +369,8 @@ class ServiceContainer(object):
         _log.debug('call stack for %s: %s',
                    worker_ctx, '->'.join(worker_ctx.call_id_stack))
 
-        with _log_time('ran worker %s', worker_ctx), push_worker_ctx(worker_ctx):
+        with _log_time('ran worker %s', worker_ctx), \
+             push_worker_ctx(worker_ctx):
             self._inject_dependencies(worker_ctx)
             self._worker_setup(worker_ctx)
 
