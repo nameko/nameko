98cd74329b239a4619e9cc4888c2f9d8b09fb3d1
Matt Bennett <matt@bennett.name>
2018-03-20 14:13:54 +0000

    shorten comment

--- nameko/amqp/__init__.py
+++ nameko/amqp/__init__.py
@@ -5,5 +5,5 @@ from .publish import (  # noqa: F401
 
 from amqp.transport import _AbstractTransport
 
-# backport https://github.com/celery/py-amqp/commit/7db1b2d4a5cfc546ab941697ae7035ff34645a5a#diff-e3b09014ce12269dff5eece1f9f7c082
+# backport http://bit.do/do-not-implement-del
 del _AbstractTransport.__del__
