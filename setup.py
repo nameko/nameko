22e8f7577de9663ea6cbd308508ffcd062b693ce
Matt Yule-Bennett <matt@bennett.name>
2018-05-04 13:20:55 +0100

    v2.9.0-rc0 (#541)
    * Preparing release candidate 2.9.0-rc0
    
    * Update CHANGES
    
    * spelling typo
    
    * Update spelling_wordlist.txt

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
 
 setup(
     name='nameko',
-    version='2.8.5',
+    version='2.9.0-rc0',
     description='A microservices framework for Python that lets service '
                 'developers concentrate on application logic and encourages '
                 'testability.',
