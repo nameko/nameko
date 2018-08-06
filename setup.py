77449c1d1e8a8d347ccf73aa923c86713a2f8880
Matt Yule-Bennett <matt@bennett.name>
2018-08-06 09:22:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
 
 setup(
     name='nameko',
-    version='2.9.1',
+    version='2.10.0',
     description='A microservices framework for Python that lets service '
                 'developers concentrate on application logic and encourages '
                 'testability.',
