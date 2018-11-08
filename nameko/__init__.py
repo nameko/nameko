93f5b9ba5fced723f2d4031ce30ebb7c1f76e44e
Ondrej Kohout <ondrej.kohout@gmail.com>
2018-11-08 12:52:24 +0100

    Add config setup and update managers

--- nameko/__init__.py
+++ nameko/__init__.py
@@ -1,4 +1,12 @@
-config = {}
+from copy import deepcopy
+from six.moves import UserDict
+
+
+class Config(UserDict):
+    pass
+
+
+config = Config()
 """
 It is possible to set the config by using ``--config`` switch of Nameko
 command line interface.
@@ -24,3 +32,46 @@ The config can be used straight on service definition level, e.g.::
 As a plain dictionary it is normally mutable at any point. After all,
 we're all consenting adults here.)
 """
+
+
+class config_setup:
+    """
+    A context manager for setting up a complete config for the given context
+
+    Intended to be used mainly in tests::
+
+        @pytest.yield_fixture
+        def config():
+            with config_setup({"spam": "ham"}):
+                yield
+
+    """
+
+    def __init__(self, context_config):
+        self.original_data = deepcopy(config)
+        config.clear()
+        config.update(context_config)
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        config.clear()
+        config.update(self.original_data)
+
+
+class config_update(config_setup):
+    """
+    A context manager for updating config just for the given context
+
+    Intended to be used mainly in tests:
+
+        @pytest.yield_fixture
+        def memory_rabbit_config():
+            with config_update({"AMQP_URI": "memory://"}):
+                yield
+    """
+
+    def __init__(self, context_config):
+        self.original_data = deepcopy(config)
+        config.update(context_config)
---
+++ test/test_config.py
@@ -0,0 +1,50 @@
+from nameko import config, config_setup, config_update
+
+
+def test_config_setup():
+
+    assert config == {}
+
+    config_setup({"spam": "ham"})
+
+    assert config == {"spam": "ham"}
+
+    config_setup({})
+
+    assert config == {}
+
+
+def test_config_setup_context_manager():
+
+    with config_setup({"spam": "ham"}):
+        assert config == {"spam": "ham"}
+
+    assert config == {}
+
+    with config_setup({"spam": "egg"}):
+        assert config == {"spam": "egg"}
+        with config_setup({"spam": "ham"}):
+            assert config == {"spam": "ham"}
+        assert config == {"spam": "egg"}
+
+    assert config == {}
+
+
+def test_config_setup_replaces_whole_config():
+
+    config_setup({"spam": "ham", "egg": "spam"})
+
+    with config_setup({"spam": "egg"}):
+        assert config == {"spam": "egg"}
+
+    assert config == {"spam": "ham", "egg": "spam"}
+
+
+def test_config_update():
+
+    config_setup({"spam": "ham", "egg": "spam"})
+
+    with config_update({"spam": "egg"}):
+        assert config == {"spam": "egg", "egg": "spam"}
+
+    assert config == {"spam": "ham", "egg": "spam"}
