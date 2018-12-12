79814c9b87ddd29817af8945108420039194f2ab
Ondrej Kohout <ondrej.kohout@gmail.com>
2018-12-12 18:58:56 +0100

    Test container config deprecated

--- test/test_container.py
+++ test/test_container.py
@@ -10,7 +10,7 @@ from eventlet import Timeout, sleep, spawn
 from eventlet.event import Event
 from mock import ANY, Mock, call, patch
 
-from nameko import config_setup
+from nameko import config, config_setup
 from nameko.constants import MAX_WORKERS_CONFIG_KEY
 from nameko.containers import ServiceContainer, get_service_name
 from nameko.exceptions import ConfigurationError
@@ -561,3 +561,12 @@ def test_logging_managed_threads(container, logger):
     assert call("killing managed thread `%s`", "wait") in call_args_list
     assert call("killing managed thread `%s`", "<unknown>") in call_args_list
     assert call("killing managed thread `%s`", "named") in call_args_list
+
+
+def test_container_config_deprecated(container):
+
+    with config_setup({'spam': 'ham'}):
+        with patch('nameko.containers.warnings') as warnings:
+            container.config == config
+
+    assert warnings.warn.call_count
