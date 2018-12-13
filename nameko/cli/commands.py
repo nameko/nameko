9d458713e46a0b7695c64b3664bc9a1f5683e31a
Ondrej Kohout <ondrej.kohout@gmail.com>
2018-12-13 13:17:50 +0100

    Remove forgotten broker default

--- nameko/cli/commands.py
+++ nameko/cli/commands.py
@@ -84,7 +84,7 @@ class Run(Command):
             help='python path to one or more service classes to run')
 
         parser.add_argument(
-            '--broker', default='pyamqp://guest:guest@localhost',
+            '--broker', default=None,
             help=(
                 'Deprecated option for setting up RabbitMQ broker URI. '
                 'Use --define or --config and set AMQP_URI instead.'
@@ -121,7 +121,7 @@ class Shell(Command):
     @classmethod
     def init_parser(cls, parser):
         parser.add_argument(
-            '--broker', default='pyamqp://guest:guest@localhost',
+            '--broker', default=None,
             help=(
                 'Deprecated option for setting up RabbitMQ broker URI. '
                 'Use --define or --config and set up AMQP_URI instead.'
