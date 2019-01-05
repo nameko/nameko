b0131f5deeb352b1e140ce400fa61a0cb3048d5e
Matt Yule-Bennett <matt@bennett.name>
2019-01-05 17:05:38 +0000

    Release v3.0.0-rc3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
 
 setup(
     name='nameko',
-    version='3.0.0-rc2',
+    version='3.0.0-rc3',
     description='A microservices framework for Python that lets service '
                 'developers concentrate on application logic and encourages '
                 'testability.',
