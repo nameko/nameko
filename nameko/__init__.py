662b0b4831df81ba990cff06e955889cc8818cf1
Ondrej Kohout <ondrej.kohout@gmail.com>
2019-01-12 14:06:05 +0100

    Better config API
    * Move `update_config` and `setup_config` helper methods from module and
      place them on config object itself.
    * Rename both helper context managers to `patch` and use `clear` argument
      of `patch` to replace the whole config.
    * Keep `setup` as simple method only (also straight on `config`)

--- nameko/__init__.py
+++ nameko/__init__.py
@@ -1,8 +1,76 @@
+from collections import UserDict
 from copy import deepcopy
+from six import wraps
 
 
-config = {}
+class Config(UserDict):
+
+    def setup(self, config):
+        self.data.clear()
+        self.data.update(config)
+
+    def patch(self, context_config, clear=False):
+        """
+        A context manager and decorator for updating config just for the given context
+        or decorated function execution.
+
+        Intended to be used mainly in tests:
+
+            @pytest.yield_fixture
+            def memory_rabbit_config():
+                with nameko.config.patch({"AMQP_URI": "memory://"}):
+                    yield
+
+            @nameko.config.patch({"AMQP_URI": "memory://"})
+            def test_spam():
+                assert nameko.config["AMQP_URI"] == "memory://"
+
+        If `clear` is set to True then the config is completely replaced with the given
+        `context_config` otherwise it's only updated.
+
+        """
+
+        config = self.data
+
+        class Patcher:
+
+            def __call__(self, func):
+                @wraps(func)
+                def wrapper(*args, **kwargs):
+                    self.original_config = deepcopy(config)
+                    if self.clear:
+                        config.clear()
+                    config.update(self.context_config)
+                    try:
+                        result = func(*args, **kwargs)
+                    finally:
+                        config.clear()
+                        config.update(self.original_config)
+                    return result
+                return wrapper
+
+            def __init__(self, context_config, clear=False):
+                self.clear = clear
+                self.context_config = context_config
+
+            def __enter__(self):
+                self.original_config = deepcopy(config)
+                if self.clear:
+                    config.clear()
+                config.update(self.context_config)
+
+            def __exit__(self, exc_type, exc_value, traceback):
+                config.clear()
+                config.update(self.original_config)
+                pass
+
+        return Patcher(context_config, clear=clear)
+
+
+config = Config()
 """
+A dictionary holding and managing configuration
+
 It is possible to set the config by using ``--config`` switch of Nameko
 command line interface.
 
@@ -27,46 +95,3 @@ The config can be used straight on service definition level, e.g.::
 As a plain dictionary it is normally mutable at any point. After all,
 we're all consenting adults here.)
 """
-
-
-class setup_config:
-    """
-    A context manager for setting up a complete config for the given context
-
-    Intended to be used mainly in tests::
-
-        @pytest.yield_fixture
-        def config():
-            with setup_config({"spam": "ham"}):
-                yield
-
-    """
-
-    def __init__(self, context_config):
-        self.original_data = deepcopy(config)
-        config.clear()
-        config.update(context_config)
-
-    def __enter__(self):
-        pass
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        config.clear()
-        config.update(self.original_data)
-
-
-class update_config(setup_config):
-    """
-    A context manager for updating config just for the given context
-
-    Intended to be used mainly in tests:
-
-        @pytest.yield_fixture
-        def memory_rabbit_config():
-            with update_config({"AMQP_URI": "memory://"}):
-                yield
-    """
-
-    def __init__(self, context_config):
-        self.original_data = deepcopy(config)
-        config.update(context_config)
