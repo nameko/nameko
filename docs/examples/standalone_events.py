7fd6c994c3d599449d1be2132016499661885b7f
Ondrej Kohout <ondrej.kohout@gmail.com>
2019-02-06 17:03:40 +0100

    Better standalone example with no config

--- docs/examples/standalone_events.py
+++ docs/examples/standalone_events.py
@@ -1,9 +1,4 @@
-from nameko import config
 from nameko.standalone.events import event_dispatcher
 
-config.setup({
-    'AMQP_URI': AMQP_URI  # e.g. "pyamqp://guest:guest@localhost"
-})
-
-dispatch = event_dispatcher()
+dispatch = event_dispatcher(uri=AMQP_URI)  # e.g. "pyamqp://guest:guest@localhost"
 dispatch("service_a", "event_type", "payløad")
