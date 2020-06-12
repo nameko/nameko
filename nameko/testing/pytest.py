6b97df7b38dea2c2aa33ad671cbe3c0b5c35ab35
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2020-06-12 10:44:20 +0000

    Import ABC from collections.abc for Python 3 compatibility.

--- nameko/testing/pytest.py
+++ nameko/testing/pytest.py
@@ -115,7 +115,10 @@ def rabbit_manager(request):
 
 @pytest.yield_fixture(scope='session')
 def vhost_pipeline(request, rabbit_manager):
-    from collections import Iterable
+    try:
+        from collections.abc import Iterable
+    except ImportError:
+        from collections import Iterable
     from six.moves.urllib.parse import urlparse  # pylint: disable=E0401
     import random
     import socket
